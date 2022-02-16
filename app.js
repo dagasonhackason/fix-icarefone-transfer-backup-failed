e8a43a163df6def84b4a0e2d77380ad4098e7352
Gabriel Dagadu <dagasonhackason@gmail.com>
2022-02-16 19:22:39 +0000

    feat: begin implement strategy

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
 require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+
+// In newer Node.js versions where process is already global this isn't necessary.
+var process = require("process");
 
 const ICAREFONE_TRANSFER_BACKUP_FOLDER_PATH = process.env.ICAREFONE_TRANSFER_BACKUP_FOLDER_PATH;
-const OS = process.env.OS;
+const OS = process.env.OS; // Multi OS Support Comming Soon....
+
+// Loop through all the files in the backup directory
+fs.readdir(ICAREFONE_TRANSFER_BACKUP_FOLDER_PATH, function (err, files) {
+  if (err) {
+    console.error("fs.readdir() -> Couldn't hit the directory =>", err);
+    process.exit(1);
+  }
+
+  files.forEach(function (file, index) {
+    // Make one pass and make the file complete
+    var loopOneCurrentFileFolderCursor = path.join(ICAREFONE_TRANSFER_BACKUP_FOLDER_PATH, file);
+
+    fs.stat(loopOneCurrentFileFolderCursor, function (error, stat) {
+      if (error) {
+        console.error("Error stating file.", error);
+        return;
+      }
+
+      if (stat.isFile())
+        console.log("'%s' is a file.", loopOneCurrentFileFolderCursor);
+      else if (stat.isDirectory())
+        console.log("'%s' is a directory.", loopOneCurrentFileFolderCursor);
+    });
+  });
+});

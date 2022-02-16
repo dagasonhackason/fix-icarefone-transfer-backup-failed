4432696c235d0e23ba8f5c1d7f16559711a98066
Gabriel Dagadu <dagasonhackason@gmail.com>
2022-02-16 20:05:11 +0000

    refactor: approach

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var process = require("process");
 const ICAREFONE_TRANSFER_BACKUP_FOLDER_PATH = process.env.ICAREFONE_TRANSFER_BACKUP_FOLDER_PATH;
 const OS = process.env.OS; // Multi OS Support Comming Soon.... Right now only Android backup fix is supported
 
+console.log("Selected OS is =>", OS);
+
 const AndroidLocalFilesMap = []; // Will hold all Android data built from backup to complete the backup process even though it failed.
 const IOSLocalFilesMap = []; // Will hold all IOS data built from backup to complete the backup process even though it failed.
 
@@ -26,15 +28,51 @@ fs.readdir(ICAREFONE_TRANSFER_BACKUP_FOLDER_PATH, function (err, files) {
 
     fs.stat(loopOneCurrentFileFolderCursor, function (error, stat) {
         if (error) {
-            console.error("Error stating file.", error);
+            console.error("Error stating file =>", error);
             return;
         }
+
+        const loopOneCurrent = path.basename(loopOneCurrentFileFolderCursor);
 
         if (stat.isFile()) {
             console.log("Now -> @ '%s' => file.", loopOneCurrentFileFolderCursor);
         } else if (stat.isDirectory()) {
             console.log("Now -> @ '%s' => directory.", loopOneCurrentFileFolderCursor);
             console.log("About this directory =>", stat);
+
+            if(loopOneCurrent === "Android") {
+                files.forEach(function (file, index) {
+                    var loopTwoCurrentFileFolderCursor = path.join(ICAREFONE_TRANSFER_BACKUP_FOLDER_PATH, file);
+                
+                    fs.stat(loopTwoCurrentFileFolderCursor, function (error, stat) {
+                        if (error) {
+                            console.error("Error stating file =>", error);
+                            return;
+                        }
+                
+                        const loopTwoCurrent = path.basename(loopTwoCurrentFileFolderCursor);
+                
+                        if (stat.isFile()) {
+                            console.log("Now -> @ '%s' => file.", loopTwoCurrentFileFolderCursor);
+                        } else if (stat.isDirectory()) {
+                            console.log("Now -> @ '%s' => directory.", loopTwoCurrentFileFolderCursor);
+                            console.log("About this directory =>", stat);
+                
+                            if(loopTwoCurrent === "Media") {
+                                
+                            }
+
+                            if(loopTwoCurrent === "Databases") {
+                                
+                            }
+
+                            if(loopTwoCurrent === "Backups") {
+                                
+                            }
+                        }
+                    });
+                });
+            }
         }
     });
   });
